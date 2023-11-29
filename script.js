be16abb0421fbdd0e1ba1719dba4394af8db3659
awesomegamergame <68134172+awesomegamergame@users.noreply.github.com>
2023-11-29 03:05:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function unwrapPresent() {
   const present = document.getElementById('present');
-  present.classList.add('unwrapped');
-
-  // Simulate unwrapping delay (you can replace this with an actual AJAX request to load another HTML file)
-  setTimeout(() => {
-    // Replace the content with another HTML file
-    present.innerHTML = '<iframe src="anotherPage.html" width="100%" height="100%"></iframe>';
-  }, 500);
+  
+  if (!present.classList.contains('unwrapped')) {
+    present.classList.add('unwrapped');
+    // Simulate unwrapping delay (you can replace this with an actual AJAX request to load another HTML file)
+    setTimeout(() => {
+      // Replace the content with another HTML file
+      present.innerHTML = '<iframe src="anotherPage.html" width="100%" height="100%"></iframe>';
+    }, 500);
+  } else {
+    present.classList.remove('unwrapped');
+    // Reset the content to the original state
+    present.innerHTML = '🎁 Click to unwrap!';
+  }
 }
